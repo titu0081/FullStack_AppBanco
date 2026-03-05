bb74e73b7bae7a0fbe8ceb87a4eeb8ce7f9a4891
titu0081 <101371992+titu0081@users.noreply.github.com>
2026-03-05 05:19:57 -0500

    Cambios finales

--- src/app/pages/cuentas/cuentas.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cuentas',
-  imports: [],
-  templateUrl: './cuentas.html',
-  styleUrl: './cuentas.scss',
-})
-export class Cuentas {}
--- src/app/pages/movimientos/movimientos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-movimientos',
-  imports: [],
-  templateUrl: './movimientos.html',
-  styleUrl: './movimientos.scss',
-})
-export class Movimientos {}
